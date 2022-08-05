a1b1977bbbbb203463332a8a31b3bdd5fb6e7833
kukuh pribadi <cristiankukuh27@gmail.com>
2022-08-05 20:46:26 +0700

    test hydrogen

---
+++ hydrogen.config.ts
@@ -0,0 +1,18 @@
+import {defineConfig, CookieSessionStorage} from '@shopify/hydrogen/config';
+
+export default defineConfig({
+  shopify: {
+    defaultCountryCode: 'US',
+    defaultLanguageCode: 'EN',
+    storeDomain: 'urban-city-barbershop2.myshopify.com',
+    storefrontToken: '8e88b6ee5142f2070d039cd5862662a3',
+    storefrontApiVersion: '2022-07',
+  },
+  session: CookieSessionStorage('__session', {
+    path: '/',
+    httpOnly: true,
+    secure: import.meta.env.PROD,
+    sameSite: 'Strict',
+    maxAge: 60 * 60 * 24 * 30,
+  }),
+});
